868fafed92715ab00b3f8e208fc1c77e3961e375
berna39 <kalemajoseph39@gmail.com>
2022-09-06 22:45:31 +0200

    Is prime number feature

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+app.get('/isprime/:number', (req, res) => {
+    result = isNumberPrime(req.params.number);
+    res.send(result);
+});
+
 app.listen(port, (err) => {
     if(!err) console.log(`Server listning on ${port}`);
     else console.error(err);
 });
+
+function isNumberPrime(number){
+
+    startTime = new Date();
+    endTime = new Date();
+    isPrime = true;
+
+    for(let i = 3; i < number; i++){
+        console.log(i);
+        if(number % i === 0){
+            endTime = new Date();
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime) endTime = new Date();
+
+    return {
+        "number": number,
+        "time": endTime.getTime() - startTime.getTime(),
+        "isPrime": isPrime
+    };
+}
 
